e13e2633df08f42b6a46ba415814933a456efd64
HyeJeong <rucy011121@gmail.com>
2024-05-09 20:10:07 +0900

    :sparkles: SignupDto.java와UserTestController.java 작성

--- likelion/src/main/java/com/example/likelion/test/SignupDto.java
+++ likelion/src/main/java/com/example/likelion/test/SignupDto.java
@@ -1,2 +1,20 @@
-package com.example.likelion.test;public class SignupDto {
+package com.example.likelion.test;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+//요청을 받는 Dto에는 builder 필요 없음
+public class SignupDto {
+
+    @NotEmpty(message = "userId는 필수 값입니다.")//빈 값이 들어올 수 없다 -> 비어 있을 수 없습니다!
+    private String userId;
+    private String email;
+    private String password;
 }
